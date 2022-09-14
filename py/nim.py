edff0e664baaef93e07384618229f6af36baa8fe
Christine Marra <96950263+chrismarra21@users.noreply.github.com>
2022-09-13 22:38:46 -0400

    Update nim.py

--- py/nim.py
+++ py/nim.py
@@ -1,1 +1,66 @@
+# https://py3.codeskulptor.org/#user307_VR9Kig4dOZ_0.py
+# Christine Marra
+# CSCI 77800 Fall 2022
+# collaborators: Dave Ciolino-Volano
+# consulted: 
 
+import random
+
+stones = 12
+stonesTaken = 0
+player = ("Human")
+print ("Stones = ", stones)
+while (stones > 0):
+    if(player=="Human" and stones >= 3):
+        print("How many stones would you like 1, 2 or 3?")
+        stonesTaken=int(input())			
+        while(stonesTaken > 3 or stonesTaken < 1):
+            print ("Woops! Please select 1, 2 or 3 only!")
+            stonesTaken=int(input())
+        player = "Human"
+        stones = stones - stonesTaken
+        print("\nStones left: ", stones)
+
+        if (player=="Human" and stones == 2):
+            print("How many stones would you like 1 or 2?")
+            stonesTaken=int(input())			
+            while(stonesTaken > 2 or stonesTaken < 1):
+                print ("Woops! Please select 1 or 2 only!")
+                stonesTaken=int(input())
+            player = "Human"
+            stones = stones - stonesTaken
+            print("\nStones left: ", stones)
+            if (stones < 1):
+                break
+
+    player = "computer"
+
+    if(player=="computer" and stones >= 3):
+        stonesTaken = random.randint(1,3)
+        print("\nThe computer takes", stonesTaken," stones.")
+        stones = stones - stonesTaken
+        print("\nStones left: ", stones)
+        if (stones < 1):
+            break
+        player = "human"
+
+
+    if(player=="computer" and (stones == 2 or stones == 1)):
+        stonesTaken = random.randint(1,2)
+        print("The computer takes", stonesTaken," stones.")
+        stones = stones - stonesTaken
+        print("Stones left: ", stones)
+
+        if (stones < 1):
+            break
+    player = "Human"
+
+    if(player=="computer" and stones == 1):
+        stonesTaken = 1
+        print("The computer takes", stonesTaken, " stones.")
+        stones = stones - stonesTaken
+        print("Stones left: ", stones,)
+        if (stones < 1):
+            break
+    player = "Human"
+print ("the ", player, " wins!")
